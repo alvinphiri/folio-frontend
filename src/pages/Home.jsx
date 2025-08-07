2c4f5ebbed4a9079710fc72d6724361b4700fb8f
alvin phiri <94138456+alvinphiri@users.noreply.github.com>
2025-08-07 10:26:05 +0200

    done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,9 +13,9 @@ export default function Home() {
       <PageWrapper>
         <div className="min-h-screen flex flex-col items-center text-center justify-center bg-gray-950 text-white px-4">
           <h1 className="text-4xl md:text-6xl max-w-[65%] font-bold mb-4">Alvin <br/> Coder, Builder, your all around Systems guy</h1>
-          <p className="text-xl text-gray-400 max-w-[65%] mb-6">I make things work, fix what’s broken, and keep it clean while I’m at it.
-From Zimbabwe to the world. Cybersecurity certified. Backend and frontend fluent.
-Wanna see what I can do?</p>
+          <p className="text-xl text-gray-400 max-w-[65%] mb-6">Every project is a lab, every bug a lesson.
+        learning how AI reshapes our thinking, one commit at a time.
+What can i do for you?</p>
           <div className="mt-8 flex w-full justify-center space-x-4">
               <Link to="/projects" className="bg-indigo-600 hover:bg-indigo-700 text-white px-6 py-3 rounded-full transition mr-4">View My Work</Link>
               <Link to="/resume" className="bg-green-600 hover:bg-green-700 text-white px-6 py-3 rounded-full transition">View Resume</Link>
